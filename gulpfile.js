b1aa9cf09462307cc966ddb537a06fd58573602a
Alexei Dvorac <lexhxas@gmail.com>
2016-07-17 09:08:35 +0300

    Fix copy-assets task. Load purecss into page.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
+var changed = require('gulp-changed');
 
 var paths = {
     pug_templates: ['templates/**/*.pug'],
@@ -16,13 +17,21 @@ gulp.task('defaut', function() {
 
 gulp.task('compile-templates', function buildHTML() {
     return gulp.src(paths.pug_templates)
+        .pipe(changed(paths.destination))
         .pipe(pug())
         .pipe(gulp.dest(paths.destination));
 });
 
 gulp.task('copy-to-destination', function() {
-    return gulp.src(assets_paths)
-        .pipe(gulp.dest(paths.destination + '/assets'));
+    gulp.src(paths.scripts)
+        .pipe(changed(paths.destination + '/assets/js'))
+        .pipe(gulp.dest(paths.destination + '/assets/js'));
+    gulp.src(paths.images)
+        .pipe(changed(paths.destination + '/assets/images'))
+        .pipe(gulp.dest(paths.destination + '/assets/images'));
+    gulp.src(paths.css)
+        .pipe(changed(paths.destination + '/assets/css'))
+        .pipe(gulp.dest(paths.destination + '/assets/css'));
 });
 
 gulp.task('watch', function() {
