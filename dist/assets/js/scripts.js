efe32011c1dac75b0d75960acbf41a6b0271385b
Alexei Dvorac <lexhxas@gmail.com>
2016-07-24 17:07:35 +0300

    Make text and rotator responsive. Clean-up rotator JS

--- dist/assets/js/scripts.js
+++ dist/assets/js/scripts.js
@@ -1,65 +1,52 @@
 /* eslint-env jquery */
 function externalLinks() {
     if (!document.getElementsByTagName) return;
-    var anchors = document.getElementsByTagName("a");
+    var anchors = document.getElementsByTagName('a');
     for (var i = 0; i < anchors.length; i++) {
         var anchor = anchors[i];
-        if (anchor.getAttribute("href") &&
-            anchor.getAttribute("rel") == "external")
-            anchor.target = "_blank";
+        if (anchor.getAttribute('href') &&
+            anchor.getAttribute('rel') == 'external')
+            anchor.target = '_blank';
     }
 }
 window.onload = externalLinks;
 
 function theRotator() {
-    //Set the opacity of all images to 0
-    $('div.rotator ul li').css({
-        opacity: 0.0
-    });
-
-    //Get the first image and display it (gets set to full opacity)
-    $('div.rotator ul li:first').css({
-        opacity: 1.0
-    });
-
     //Call the rotator function to run the slideshow, 6000 = change to next image after 6 seconds
 
-    setInterval('rotate()', 6000);
+    setInterval(rotate, 6000);
 
 }
 
 function rotate() {
-    //Get the first image
-    var current = ($('div.rotator ul li.show') ? $('div.rotator ul li.show') : $('div.rotator ul li:first'));
+    var $slides = $('.rotator ul li'),
+        $current = $slides.filter('.show'),
+        $next = $slides.first(),
+        slide_heights = $slides.map(function(i, slide) {
+            return $(slide).height();
+        }),
+        max_slide_height = Math.max.apply(null, slide_heights);
 
-    if (current.length == 0) current = $('div.rotator ul li:first');
+    $slides.parent().height(max_slide_height);
 
-    //Get next image, when it reaches the end, rotate it back to the first image
-    var next = ((current.next().length) ? ((current.next().hasClass('show')) ? $('div.rotator ul li:first') : current.next()) : $('div.rotator ul li:first'));
+    if (($slides.index($current) + 1) < $slides.length) {
+        $next = $current.next();
+    }
 
-    //Un-comment the 3 lines below to get the images in random order
-
-    //var sibs = current.siblings();
-    //var rndNum = Math.floor(Math.random() * sibs.length );
-    //var next = $( sibs[ rndNum ] );
 
 
     //Set the fade in effect for the next image, the show class has higher z-index
-    next.css({
-            opacity: 0.0
-        })
-        .addClass('show')
+    $next.addClass('show')
         .animate({
-            opacity: 1.0
+            opacity: 1
         }, 1000);
 
     //Hide the current image
-    current.animate({
-            opacity: 0.0
+    $current.animate({
+            opacity: 0
         }, 1000)
         .removeClass('show');
-
-};
+}
 
 
 
