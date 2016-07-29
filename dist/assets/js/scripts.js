c3b067756d6fe871a90f75c38759e9b9d305b5a4
Alexei Dvorac <lexhxas@gmail.com>
2016-07-29 15:02:14 +0300

    Remove all 'rotator' code. Replace all rotators with ideal-image-slider. Remove jquery dependency. Remove jquery.

--- dist/assets/js/scripts.js
+++ dist/assets/js/scripts.js
@@ -1,64 +1,15 @@
-/* eslint-env jquery */
-
-$(document).ready(initRotator);
-$(window).on('load', externalLinks);
-$(window).on('load resize', setupRotatorHeight);
-
-
-function externalLinks() {
-    if (!document.getElementsByTagName) return;
-    var anchors = document.getElementsByTagName('a');
-    for (var i = 0; i < anchors.length; i++) {
-        var anchor = anchors[i];
-        if (anchor.getAttribute('href') &&
-            anchor.getAttribute('rel') == 'external')
-            anchor.target = '_blank';
-    }
-}
-
-function initRotator() {
-    setupRotatorHeight();
-    //Call the rotator function to run the slideshow, 6000 = change to next image after 6 seconds
-    setInterval(rotate, 6000);
-    $('div.rotator').fadeIn(1000);
-    $('div.rotator ul li').fadeIn(1000); // tweak for IE
-}
-
-function setupRotatorHeight() {
-    var $slides = $('.rotator ul li'),
-        slide_heights = $slides.map(function(i, slide) {
-            return $(slide).height();
-        }),
-        max_slide_height = Math.max.apply(null, slide_heights);
-
-    $slides.parent().height(max_slide_height);
-}
-
-function rotate() {
-    var $slides = $('.rotator ul li'),
-        $current = $slides.filter('.show'),
-        $next = $slides.first(),
-        slide_heights = $slides.map(function(i, slide) {
-            return $(slide).height();
-        }),
-        max_slide_height = Math.max.apply(null, slide_heights);
-
-    $slides.parent().height(max_slide_height);
-
-    if (($slides.index($current) + 1) < $slides.length) {
-        $next = $current.next();
+'use strict';
+(function() {
+    function externalLinks() {
+        if (!document.getElementsByTagName) return;
+        var anchors = document.getElementsByTagName('a');
+        for (var i = 0; i < anchors.length; i++) {
+            var anchor = anchors[i];
+            if (anchor.getAttribute('href') && anchor.getAttribute('rel') == 'external') {
+                anchor.target = '_blank';
+            }
+        }
     }
 
-    //Set the fade in effect for the next image, the show class has higher z-index
-    $next.addClass('show')
-        .animate({
-            opacity: 1
-        }, 1000);
-
-    //Hide the current image
-    $current.animate({
-            opacity: 0
-        }, 1000)
-        .removeClass('show');
-}
-/*Marshall Smith*/
+    window.onload = externalLinks;
+})();
--- dist/assets/js/slider.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-(function() {
-    var slider = new IdealImageSlider.Slider({
-        selector: '#imagine-slider',
-        effect: 'fade'
-    });
-})();
